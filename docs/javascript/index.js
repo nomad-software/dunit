7a4a4608c2428621eb8725fc7616397a5a266caa
Gary Willoughby <dev@kalekold.net>
2013-11-17 15:19:04 +0000

    Added an index to the dynamically generated documentation and tweaked the css for better readability.

---
+++ docs/javascript/index.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+	var container = $('<div id="index"><h2>Index</h2></div>');
+	var list      = $('<ul id="list"></ul>');
+
+	// Create the list.
+	var anchorIteration = 0;
+	$("h2 .symbol").each(function(){
+		var text = $(this).text();
+		$(this).before('<a name="' + text + anchorIteration + '"></a>');
+		list.append('<li class="item"><a href="#' + text + anchorIteration + '">' + text + '</li>');
+		anchorIteration++;
+	});
+
+	list.appendTo(container);
+	container.prependTo("body");
+
+	// Handle page scrolling.
+	var indexTop = $('#index').offset().top;
+	$(window).scroll(function(){
+
+		var windowTop = $(window).scrollTop();
+
+		if (indexTop < windowTop) {
+			$("#index").css({position:"fixed", top:0, left:0});
+		}
+		else {
+			$("#index").css({position:"absolute"});
+		}
+	 });
+});
